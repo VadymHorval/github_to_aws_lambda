8a0ef96e73411c9955819d5c8c310f5e8a411d43
Vadym <vadym.horval@gmail.com>
2021-08-15 00:57:42 +0300

    test print AWS event

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,6 @@
 def lambda_handler(event, context):
+    text = json.dumps(event, sort_keys=True, indent=3)
     print('Hello World!')
-    return 'Hello World!'     
+    #return 'Hello World!'
+    return text
     
