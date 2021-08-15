5cef52088663f9d373c88333acb1a2406a91d105
Vadym <vadym.horval@gmail.com>
2021-08-15 13:30:04 +0300

    add json

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,10 @@
-#import json
+import json
+
+
 def lambda_handler(event, context):
-    #text = json.dumps(event, sort_keys=True, indent=3)
-    print('Hello World!')
-    for i in range(10):
-        print('iterator',i)
-        yield i
-    return 'Hello World!'
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello World')
+    }
     
