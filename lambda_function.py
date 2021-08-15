b30564c32bd9eedb22b51d989567172f80bb9caa
Vadym <vadym.horval@gmail.com>
2021-08-15 15:48:54 +0300

    add json post

--- lambda_function.py
+++ lambda_function.py
@@ -4,12 +4,12 @@ import urllib3
 
 def lambda_handler(event, context):
 
-    http = urllib3.PoolManager()
-    resp = http.request(
-        "POST",
-        "https://82js1wszcd.execute-api.eu-central-1.amazonaws.com/default/github_to_aws_lambda",
-        fields={"body": "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"}  # Add custom form fields
-    )
+    # http = urllib3.PoolManager()
+    # resp = http.request(
+    #     "POST",
+    #     "https://82js1wszcd.execute-api.eu-central-1.amazonaws.com/default/github_to_aws_lambda",
+    #     fields={"body": "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"}  # Add custom form fields
+    # )
 
     return {
         'statusCode': 200,
