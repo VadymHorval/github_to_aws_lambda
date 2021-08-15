acc5b5508c297699f44dd93d520d377054cadd76
Vadym <vadym.horval@gmail.com>
2021-08-15 15:28:57 +0300

    add json post

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,16 @@
 import json
-from botocore.vendored import requests
+import urllib3
 
 
 def lambda_handler(event, context):
-    r = requests.post('https://82js1wszcd.execute-api.eu-central-1.amazonaws.com/default/github_to_aws_lambda', data={'body': '!!!!!!!!!!!!!!!!!!!!!!!!!'})
+
+    http = urllib3.PoolManager()
+    resp = http.request(
+        "POST",
+        "https://82js1wszcd.execute-api.eu-central-1.amazonaws.com/default/github_to_aws_lambda",
+        fields={"body": "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"}  # Add custom form fields
+    )
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello World')
