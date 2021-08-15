86907ed040c93bbcc1e5103dfa83b27c452a33ef
Vadym <vadym.horval@gmail.com>
2021-08-15 15:13:03 +0300

    add json post

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
 import json
+import requests
 
 
 def lambda_handler(event, context):
-
+    r = requests.post('https://82js1wszcd.execute-api.eu-central-1.amazonaws.com/default/github_to_aws_lambda', data={'body': '!!!!!!!!!!!!!!!!!!!!!!!!!'})
     return {
         'statusCode': 200,
         'body': json.dumps('Hello World')
